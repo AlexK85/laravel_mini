4d2da53f6bc6546e7a586804083f486bffa7923f
AlexK85 <saharr.k@mail.ru>
2022-04-03 17:02:08 +0300

    new update2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Телефонная книга!!!!
+                Телефонная книга!!!!124
             </div>
 
             <div class="links">
