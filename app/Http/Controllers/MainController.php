4b3fe9f8c4650e73c30eb7101c92136719fcb876
AlexK85 <saharr.k@mail.ru>
2022-04-03 18:41:48 +0300

    Сортировка в алфавитном порядке + вывод 10 контактов при помощи метода paginate

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,8 +11,14 @@ class MainController extends Controller
     // метод получает все записи из телефонного справочника 
     public function index()
     {
-        $users = Phonebook::all();
+
+        // $users = Phonebook::all();
+
+        //$users = Phonebook::orderBy('name')->get(); //сортировка в алффвитном пордке 
+        $users = Phonebook::orderBy('name')->paginate(10); //сортировка в алффвитном пордке 
+
         //dd($users); // выводит коллекцию из 100 записей
         return view('home', compact('users')); // подключаем вид 'home' и при помощи compact('users') передаём users
+
     }
 }
